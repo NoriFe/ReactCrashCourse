3edc18421a7564aa184a4785d74291a40b029a6d
Norbert Skwierczynski <norbert.fe.dev@gmail.com>
2024-03-18 20:15:54 +0000

    adding hero component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
-import React from 'react'
+
+
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+      <Navbar />
+      <Hero />
+    </>
   )
 }
 
