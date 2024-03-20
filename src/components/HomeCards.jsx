99ae22a5fd9a61c539136ea9aefdf2c4e1632f3e
Norbert Skwierczynski <norbert.fe.dev@gmail.com>
2024-03-20 11:00:02 +0000

    creating json for cards

--- src/components/HomeCards.jsx
+++ src/components/HomeCards.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-
+import Card from './Card'
 const HomeCards = () => {
   return (
     <section class="py-4">
 <div class="container-xl lg:container m-auto">
   <div class="grid grid-cols-1 md:grid-cols-2 gap-4 p-4 rounded-lg">
-    <div class="bg-gray-100 p-6 rounded-lg shadow-md">
+   <Card>  
       <h2 class="text-2xl font-bold">For Developers</h2>
       <p class="mt-2 mb-4">
         Browse our React jobs and start your career today
@@ -16,8 +16,9 @@ const HomeCards = () => {
       >
         Browse Jobs
       </a>
-    </div>
-    <div class="bg-indigo-100 p-6 rounded-lg shadow-md">
+    
+   </Card>
+    <Card bg='bg-indigo-200'>
       <h2 class="text-2xl font-bold">For Employers</h2>
       <p class="mt-2 mb-4">
         List your job to find the perfect developer for the role
@@ -28,7 +29,7 @@ const HomeCards = () => {
       >
         Add Job
       </a>
-    </div>
+    </Card>
   </div>
 </div>
 </section>
