76fb3cfb0b55c6edb54c1fa46c4a1487b660fd12
Norbert Skwierczynski <norbert.fe.dev@gmail.com>
2024-03-22 11:48:02 +0000

    added courses page to app

--- src/pages/CoursesPage.jsx
+++ src/pages/CoursesPage.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from 'react';
+import Courses from '../components/Courses';
 
 const CoursesPage = () => {
   return (
-    <div>CoursesPage</div>
-  )
-}
+    <section className="bg-blue-50 px-4 py-6">      
+      <Courses />
+    </section>
+
+
+ ) 
+};
 
 export default CoursesPage
